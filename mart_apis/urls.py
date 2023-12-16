52bccb17c7f57496efa370803b61d4c58af286cd
shaileshg <shaileshg@tradeindia.com>
2023-12-16 23:18:36 +0530

    new chage

--- mart_apis/urls.py
+++ mart_apis/urls.py
@@ -22,6 +22,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('martcrm/',include('martcrm.urls')),
     re_path(r'^$', TemplateView.as_view(template_name='static_pages/index.html'), name='home'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
