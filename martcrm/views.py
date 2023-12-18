d7e695d8481e6cca6869aa19a6ec9d8279627860
shaileshgola1987 <shailesh.gola@outlook.com>
2023-12-18 08:15:46 +0530

    Newe Changes

--- martcrm/views.py
+++ martcrm/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def index(request):
+    # return redirect('mart-trade/login.html')
     return render(request,'mart-trade/login.html')
+
+def login(request):
+    pass
